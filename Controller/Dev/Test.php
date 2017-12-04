1db2e3dbb2aebfd9958071ae0edbefb204644384
Mikko Ropanen <mropanen@gmail.com>
2017-12-04 23:31:14 +0200

    Added checkout session to dev controller

--- Controller/Dev/Test.php
+++ Controller/Dev/Test.php
@@ -17,18 +17,23 @@ class Test extends \Magento\Framework\App\Action\Action
 
     /** @var \Magento\Payment\Api\PaymentMethodListInterface  */
     protected $_paymentMethodList;
+    /** @var \Magento\Checkout\Model\Session  */
+    protected $_session;
 
     public function __construct(
         Context $context,
-        \Magento\Payment\Api\PaymentMethodListInterface $paymentMethodList
+        \Magento\Payment\Api\PaymentMethodListInterface $paymentMethodList,
+        \Magento\Checkout\Model\Session $session
     )
     {
+        $this->_session = $session;
         $this->_paymentMethodList = $paymentMethodList;
         parent::__construct($context);
     }
 
     public function execute()
     {
+        $quote = $this->_session->getQuote();
         echo '<pre>';
         var_dump($this->_paymentMethodList->getActiveList(1));
         echo '</pre>';
